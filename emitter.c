2e395d668ee83b045a15b534050a5b6032104d27
luchetto93 <guerrini_luc93@hotmail.com>
2016-10-10 21:15:11 +0200

    emitter.c

---
+++ emitter.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+int main()
+{
+    int inputStream[1
+    char buf[M];
+    struct sockaddr_un sa;
+    strncpy(sa.sun_path,SOCKNAME,UNIX_PATH_MAX);
+    sa.sun_family=AF_UNIX;
+    fd_skt=socket(AF_UNIX,SOCK_STREAM,0);
+    
+
+}
